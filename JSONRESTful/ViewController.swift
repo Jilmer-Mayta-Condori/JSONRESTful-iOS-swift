7882920503e19acea96d10665b2becc46bc1c905
Jilmer Mayta <jilmer.mayta@tecsup.edu.pe>
2021-06-22 01:56:18 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Jilmer Mayta on 6/22/21.
+//  Copyright © 2021 Jilmer Mayta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
